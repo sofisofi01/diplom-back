d9729f45096b7b3066ee759e062f5b7498f3c9e2
Александр <pic@MacStudio-Aleksandr.local>
2026-03-23 00:57:25 +0300

    Fix: set default schema class for drf-spectacular

--- wellness_backend/settings.py
+++ wellness_backend/settings.py
@@ -102,6 +102,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
