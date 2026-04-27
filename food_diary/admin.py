f87f5389a360bd2688d3e59d01c3bd94ad3d70ff
sofia <sofi.sofi.01@mail.ru>
2026-04-27 23:27:21 +0300

    Fix admin.py imports for nutrition plan models

--- food_diary/admin.py
+++ food_diary/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-from .models import FoodItem, FoodDiaryEntry
+from .models import FoodItem, NutritionPlan, NutritionDay, NutritionEntry
 
 @admin.register(FoodItem)
 class FoodItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'calories', 'protein', 'carbs', 'fat', 'serving_size')
     search_fields = ('name',)
 
-@admin.register(FoodDiaryEntry)
-class FoodDiaryEntryAdmin(admin.ModelAdmin):
-    list_display = ('user', 'food_item', 'meal_type', 'servings', 'date')
-    list_filter = ('meal_type', 'date')
-    search_fields = ('user__email', 'food_item__name')
+@admin.register(NutritionPlan)
+class NutritionPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'is_active', 'created_at')
+    list_filter = ('is_active',)
+    search_fields = ('user__email', 'name')
+
+@admin.register(NutritionDay)
+class NutritionDayAdmin(admin.ModelAdmin):
+    list_display = ('name', 'plan', 'day_number')
+    list_filter = ('day_number',)
+
+@admin.register(NutritionEntry)
+class NutritionEntryAdmin(admin.ModelAdmin):
+    list_display = ('food_name', 'nutrition_day', 'meal_type', 'calories', 'is_eaten')
+    list_filter = ('meal_type', 'is_eaten')
+    search_fields = ('food_name',)
