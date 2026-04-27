c4aa111f33988f9fc5fa987e4869ce9fa8269744
sofia <sofi.sofi.01@mail.ru>
2026-04-27 20:27:56 +0300

    Restore full migration operations after successful sync

--- exercises/migrations/0002_update_exercise_and_workout.py
+++ exercises/migrations/0002_update_exercise_and_workout.py
@@ -7,24 +7,24 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # migrations.AddField(
-        #     model_name='exercise',
-        #     name='calories_per_repetition',
-        #     field=models.DecimalField(decimal_places=2, default=0.0, max_digits=6),
-        # ),
-        # migrations.AddField(
-        #     model_name='exercise',
-        #     name='equipment',
-        #     field=models.JSONField(default=list, help_text='Список необходимого оборудования'),
-        # ),
+        migrations.AddField(
+            model_name='exercise',
+            name='calories_per_repetition',
+            field=models.DecimalField(decimal_places=2, default=0.0, max_digits=6),
+        ),
+        migrations.AddField(
+            model_name='exercise',
+            name='equipment',
+            field=models.JSONField(default=list, help_text='Список необходимого оборудования'),
+        ),
         migrations.AddField(
             model_name='workoutexercise',
             name='is_completed',
             field=models.BooleanField(default=False),
         ),
-        # migrations.AlterField(
-        #     model_name='exercise',
-        #     name='target_muscles',
-        #     field=models.JSONField(default=list, help_text='Список целевых мышц'),
-        # ),
+        migrations.AlterField(
+            model_name='exercise',
+            name='target_muscles',
+            field=models.JSONField(default=list, help_text='Список целевых мышц'),
+        ),
     ]
