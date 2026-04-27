63a9c9f2af979c649f7ffad8d9f08e8d1adaa19e
sofia <sofi.sofi.01@mail.ru>
2026-04-27 20:20:05 +0300

    Add manual migrations and update CI/CD

---
+++ exercises/migrations/0002_update_exercise_and_workout.py
@@ -0,0 +1,30 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('exercises', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='exercise',
+            name='calories_per_repetition',
+            field=models.DecimalField(decimal_places=2, default=0.0, max_digits=6),
+        ),
+        migrations.AddField(
+            model_name='exercise',
+            name='equipment',
+            field=models.JSONField(default=list, help_text='Список необходимого оборудования'),
+        ),
+        migrations.AddField(
+            model_name='workoutexercise',
+            name='is_completed',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AlterField(
+            model_name='exercise',
+            name='target_muscles',
+            field=models.JSONField(default=list, help_text='Список целевых мышц'),
+        ),
+    ]
