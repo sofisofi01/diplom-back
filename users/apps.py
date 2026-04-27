f0e9bcc176ff959f4611bc48c1cd6406ca1754c5
sofia <sofi.sofi.01@mail.ru>
2026-04-27 10:27:14 +0300

    security: remove temporary admin creation logic

--- users/apps.py
+++ users/apps.py
@@ -3,21 +3,3 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    def ready(self):
-        import os
-        if os.environ.get('RUN_MAIN') == 'true':  # Чтобы не запускалось дважды при авторелоаде
-            try:
-                from .models import User
-                email = 'admin@admin.com'
-                password = 'adminpassword123'
-                if not User.objects.filter(email=email).exists():
-                    User.objects.create_superuser(email=email, password=password)
-                    print("Superuser created")
-                else:
-                    user = User.objects.get(email=email)
-                    user.is_staff = True
-                    user.is_superuser = True
-                    user.save()
-            except Exception as e:
-                print(f"Error creating superuser: {e}")
